a7dd5e3774b06f278aa34ae75b1a937347e99d1f
Benson Odongo <2013odongo@gmail.com>
2025-08-05 13:47:47 +0300

    allowed hosts

--- nextalin/settings.py
+++ nextalin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*hk1a(2h2xn=9!g+)ixjh+@c+7_0kmg5w1sy=ia^+&=%4+4mgj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','nextalin.onrender.com']
 
 
 # Application definition
